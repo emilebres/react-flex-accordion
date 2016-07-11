39e8fd758689e4ce2fccb9728fbae293c919da49
Emile Bres <emile.bres@gmail.com>
2016-07-11 15:46:23 +0200

    Fix bugs in demo page

--- source/demo/App.js
+++ source/demo/App.js
@@ -1,22 +1,40 @@
 import React from 'react'
 
 import {FlexAccordionExample} from '../FlexAccordion'
-import styles from './App.css'
+
+const styles = {
+  header: {
+    backgroundColor: '#2196f3',
+    color: 'rgba(255,255,255,0.85)',
+    margin: 0,
+    padding: '1rem 0'
+  },
+
+  container: {
+    width: '800px',
+    maxWidth: '100%',
+    margin: '.5rem auto 0',
+    padding: '0 1rem',
+    textAlign: 'right'
+  },
+
+  headerLink: {
+    color: '#fff'
+  }
+}
 
 const App = () => (
   <div>
-    <header className={styles.header}>
-      <h1 className={styles.container}>React Flex Accordion</h1>
-      <div className={styles.container}>
+    <header style={styles.header}>
+      <h1 style={{...styles.container, color: 'white'}}>React Flex Accordion</h1>
+      <div style={styles.container}>
         Flex Accordion component in React
       </div>
-      <div className={styles.container}>
-        Docs and code on <a className={styles.headerLink} href='https://github.com/emilebres/react-flex-accordion/'>GitHub</a>.
+      <div style={styles.container}>
+        Docs and code on <a style={styles.headerLink} href='https://github.com/emilebres/react-flex-accordion/'>GitHub</a>.
       </div>
     </header>
-    <section className={styles.container}>
-      <FlexAccordionExample />
-    </section>
+    <FlexAccordionExample />
   </div>
 )
 
